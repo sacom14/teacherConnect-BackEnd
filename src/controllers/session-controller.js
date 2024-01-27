a635ac9147566526f57b4d197a16ab0955451d4c
Samir Comas Moral <samir.comas1@gmail.com>
2024-01-27 12:41:16 +0100

    modified the session.controller querys adding session_material request

--- src/controllers/session-controller.js
+++ src/controllers/session-controller.js
@@ -1,12 +1,12 @@
 const db = require('../config/dbConfig');
 
-const getAllSessionsByTeacher = async(req, res, next) => {
+const getAllSessionsByTeacher = async (req, res, next) => {
     try {
         // Obtener el ID del estudiante
         const teacherId = req.params.teacherId;
 
         const query = `
-            SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
+            SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_material, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
             INNER JOIN student_subject ON fk_id_student_subject = id_student_subject
             INNER JOIN subject ON fk_id_subject = id_subject
             INNER JOIN student ON fk_id_student = id_student
@@ -16,7 +16,7 @@ const getAllSessionsByTeacher = async(req, res, next) => {
 
         const [sessions, _] = await db.execute(query, [teacherId]);
 
-        if(sessions.length > 0){
+        if (sessions.length > 0) {
             res.status(200).json({ sessions });
         } else {
             res.status(200).json({ message: 'Any sessions for this teacher' });
@@ -33,17 +33,17 @@ const getSessionsByStudent = async (req, res, next) => {
         const studentId = req.params.studentId;
 
         const query = `
-            SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
+            SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_material, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
             INNER JOIN student_subject ON fk_id_student_subject = id_student_subject
             INNER JOIN subject ON fk_id_subject = id_subject
             INNER JOIN student ON fk_id_student = id_student
             INNER JOIN teacher ON fk_id_teacher = id_teacher
             WHERE fk_id_student = ?;
         `;
-        
+
         const [sessions, _] = await db.execute(query, [studentId]);
 
-        if(sessions.length > 0){
+        if (sessions.length > 0) {
             res.status(200).json({ sessions });
         } else {
             res.status(200).json({ message: 'Any sessions for this student' });
@@ -59,18 +59,18 @@ const getSessionById = async (req, res, next) => {
         //cogemos el parametro de ID
         const id = req.params.id;
 
-        const [sessions, _] = await db.execute(`SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
+        const [sessions, _] = await db.execute(`SELECT id_session, session_name, session_objective, session_start, session_end, session_tasks, session_material, session_payed, fk_id_student_subject, create_at_session, update_at_session, student_name, id_student, subject_name, id_subject FROM session
         INNER JOIN student_subject ON fk_id_student_subject = id_student_subject
         INNER JOIN subject ON fk_id_subject = id_subject
         INNER JOIN student ON fk_id_student = id_student
         INNER JOIN teacher ON fk_id_teacher = id_teacher
         WHERE id_session = ?`, [id]);
-        
-        if(sessions.length > 0){
+
+        if (sessions.length > 0) {
             res.status(200).json({ sessions });
         } else {
-        res.status(200).json({ message: 'Any session with this ID' });
-            
+            res.status(200).json({ message: 'Any session with this ID' });
+
         }
     }
     catch (error) {
@@ -81,15 +81,16 @@ const getSessionById = async (req, res, next) => {
 const addNewSession = async (req, res, next) => {
     try {
         // datos del body
-        const { sessionName, 
-            sessionObjective, 
-            sessionStart, 
-            sessionEnd, 
-            sessionTasks} = req.body;
+        const { sessionName,
+            sessionObjective,
+            sessionStart,
+            sessionEnd,
+            sessionTasks,
+            sessionMaterial } = req.body;
         console.log('body', req.body);
 
         const fkIdStudentSubject = req.params.fkIdStudentSubject;
-        console.log('params', req.params.fkIdStudentSubject);
+        console.log('id student subjet', fkIdStudentSubject);
         const sessionPayed = false;
 
         const query = `
@@ -98,18 +99,20 @@ const addNewSession = async (req, res, next) => {
                 session_objective, 
                 session_start, 
                 session_end, 
-                session_tasks, 
+                session_tasks,
+                session_material, 
                 session_payed, 
                 fk_id_student_subject
-            ) VALUES (?, ?, ?, ?, ?, ?, ?)
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         `;
         const result = await db.execute(query, [
-            sessionName, 
-            sessionObjective, 
-            sessionStart, 
-            sessionEnd, 
-            sessionTasks, 
-            sessionPayed, 
+            sessionName,
+            sessionObjective,
+            sessionStart,
+            sessionEnd,
+            sessionTasks,
+            sessionMaterial,
+            sessionPayed,
             fkIdStudentSubject
         ]);
 
@@ -124,7 +127,7 @@ const updateSession = async (req, res, next) => {
     try {
         //id de session
         const id = req.params.id;
-        const { sessionName, sessionObjective, sessionStart, sessionEnd, sessionTasks, sessionPayed, fkIdStudentSubject } = req.body;
+        const { sessionName, sessionObjective, sessionStart, sessionEnd, sessionTasks, sessionMaterial, sessionPayed, fkIdStudentSubject } = req.body;
 
         const query = `
             UPDATE session
@@ -132,24 +135,26 @@ const updateSession = async (req, res, next) => {
                 session_objective = ?, 
                 session_start = ?, 
                 session_end = ?, 
-                session_tasks = ?, 
+                session_tasks = ?,
+                session_material = ?, 
                 session_payed = ?,
                 fk_id_student_subject = ?
             WHERE id_session = ?
         `;
-        
+
         const [result] = await db.execute(query, [
-            sessionName, 
-            sessionObjective, 
-            sessionStart, 
-            sessionEnd, 
-            sessionTasks, 
+            sessionName,
+            sessionObjective,
+            sessionStart,
+            sessionEnd,
+            sessionTasks,
+            sessionMaterial,
             sessionPayed,
-            fkIdStudentSubject, 
+            fkIdStudentSubject,
             id
         ]);
 
-        if(result.affectedRows > 0){
+        if (result.affectedRows > 0) {
             res.status(200).json({ message: "Session successfully updated" });
         } else {
             res.status(404).json({ message: "Any session with this ID" });
